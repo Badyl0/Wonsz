0af01b4cd29c4a66ba98652afafd1489d083eb93
Badyl0 <marcin.badyl@gmail.com>
2018-08-01 13:37:31 +0200

    Podejście do webApp - HelloWebApp działa.

--- src/main/java/HelloWebApp/HelloWebApp.java
+++ src/main/java/HelloWebApp/HelloWebApp.java
@@ -12,16 +12,14 @@ public class HelloWebApp extends HttpServlet {
     @java.lang.Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-                super.doGet(req, resp);
     }
 
     @java.lang.Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-                super.doPost(req, resp);
                 String name = req.getParameter("name");
                 if (name == null) name = "WebApp";
                 req.setAttribute("user",name);
-                req.getRequestDispatcher("respone.jsp").forward(req,resp);
+                req.getRequestDispatcher("response.jsp").forward(req,resp);
     }
 }
